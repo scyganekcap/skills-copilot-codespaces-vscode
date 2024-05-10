47b0899b24e9082fb20a0a21c2c4244289dbbf69
scyganekcap <sebastian.cyganek@capgemini.com>
2024-05-10 08:01:09 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var skills = [
+    { name: 'HTML', level: 8 },
+    { name: 'CSS', level: 7 },
+    { name: 'Javascript', level: 6 },
+    { name: 'React', level: 5 },
+  ];
+
+  return skills;
+}
