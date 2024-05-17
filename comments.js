f40e270f2a9da432eadc902e9e2db84d093601cf
scyganekcap <sebastian.cyganek@capgemini.com>
2024-05-17 06:26:10 +0000

    Copilot third commit - 2nd try

--- comments.js
+++ comments.js
@@ -1,31 +1,35 @@
-// create web server
-const express = require('express');
-const app = express();
-// middleware
-app.use(express.json());
-// create a list of comments
-const comments = [];
-// create a new comment
-app.post('/comments', (req, res) => {
-  const comment = req.body.comment;
-  comments.push(comment);
-  res.send('Comment added');
+// Create web server
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var comments = require('./comments.json');
+
+// Use middleware to parse JSON
+app.use(bodyParser.json());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Read comments from file
+app.get('/comments', function (req, res) {
+  res.json(comments);
 });
-// get all comments
-app.get('/comments', (req, res) => {
-  res.send(comments);
+
+// Write comments to file
+app.post('/comments', function (req, res) {
+  comments.push(req.body);
+  fs.writeFile(__dirname + '/comments.json', JSON.stringify(comments, null, 4), function (err) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.json(comments);
+  });
 });
-// start web server
-app.listen(3000, () => console.log('Server is running on port 3000'));
-// Path: index.js
-// create a new comment
-const axios = require('axios');
-axios.post('http://localhost:3000/comments', {
-  comment: 'Hello world'
-}).then(res => {
-  console.log(res.data);
-});
-// get all comments
-axios.get('http://localhost:3000/comments').then(res => {
-  console.log(res.data);
+
+// Start server
+app.listen(3000, function () {
+  console.log('Server started at http://localhost:3000');
 });
